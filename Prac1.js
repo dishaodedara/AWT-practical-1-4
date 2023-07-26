697b2982a796c215f6a299efdb41a7957dc20323
Disha Odedara <96861076+dishaodedara@users.noreply.github.com>
2023-07-26 23:50:42 +0530

    Add files via upload

---
+++ Prac1.js
@@ -0,0 +1,17 @@
+
+let name = "Jay";
+let age = 15;
+let isStudent = true;
+
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
+
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+
+console.log("Sum: " + addNumbers(50, 90));
